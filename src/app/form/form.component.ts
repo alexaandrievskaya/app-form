82853925e7a41177b10e5fad6762ea9eee2e5939
alexa_andri <48593313+alexaandrievskaya@users.noreply.github.com>
2020-12-09 19:14:21 +0200

    UPD: added form validation.

---
+++ src/app/form/form.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {User} from '../user.class';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.scss']
+})
+export class FormComponent implements OnInit {
+
+  model: User = new User(0, '', '', null);
+  roles: string[] = ['Guest', 'Moderator', 'Administrator'];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  get diagnostic(): string {
+    return JSON.stringify(this.model);
+  }
+
+}
