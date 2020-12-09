d25d367b744fc672239fcb592ac47a0854bbb526
alexa_andri <48593313+alexaandrievskaya@users.noreply.github.com>
2020-12-09 20:39:39 +0200

    UPD: re-wrote validation in form component.

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,30 +1,75 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {User} from '../user.class';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss']
 })
-export class FormComponent implements OnInit {
+export class FormComponent implements OnInit, AfterViewInit {
 
   model: User = new User(1, '', '', null);
   roles: string[] = ['Guest', 'Moderator', 'Administrator'];
+
+  submitted = false;
+
+  formErrors = {
+    name: '',
+    age: ''
+  };
+  validationMessages = {
+    name: {
+      required: 'Name is required.',
+      minlength: 'Name must be at least 4 characters.'
+    },
+    age: {
+      required: 'Age is required.'
+    }
+  };
+
+  @ViewChild('userForm') userForm: NgForm;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  get diagnostic(): string {
-    return JSON.stringify(this.model);
+  ngAfterViewInit(): void {
+    this.userForm.valueChanges.subscribe((data) => this.onValueChanged());
   }
 
-  errorsLog(err): void {
-    console.log(err);
+  onValueChanged(data?: any): void {
+    if (!this.userForm) {
+      return;
+    }
+
+    const form = this.userForm.form;
+    for (const field in this.formErrors) {
+      this.formErrors[field] = '';
+
+      const control = form.get(field);
+
+      if (control && control.dirty && control.invalid) {
+        const message = this.validationMessages[field];
+
+        for (const key in control.errors) {
+          this.formErrors[field] += message[key];
+        }
+      }
+    }
   }
+
+// get diagnostic(): string {
+//   return JSON.stringify(this.model);
+// }
+
+// errorsLog(err): void {
+//   console.log(err);
+// }
 
   onSubmit(): void {
+    this.submitted = true;
     console.log('Form submitted');
   }
 
