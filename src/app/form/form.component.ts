9a61f4fea247d1c79da9ce20104122a14df869d6
alexa_andri <48593313+alexaandrievskaya@users.noreply.github.com>
2020-12-10 23:18:42 +0200

    UPD: re-wrote with reactive forms.

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,64 +1,74 @@
 import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {User} from '../user.class';
-import {NgForm} from '@angular/forms';
+import {FormControl, FormGroup, NgForm, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss']
 })
-export class FormComponent implements OnInit, AfterViewInit {
+export class FormComponent implements OnInit {
 
-  model: User = new User(1, '', '', null);
-  roles: string[] = ['Guest', 'Moderator', 'Administrator'];
+  loginForm: FormGroup; /*это форма отдельных элементов управления - группа FormControls*/
 
-  submitted = false;
-
-  formErrors = {
-    name: '',
-    age: ''
-  };
-  validationMessages = {
-    name: {
-      required: 'Name is required.',
-      minlength: 'Name must be at least 4 characters.'
-    },
-    age: {
-      required: 'Age is required.'
-    }
-  };
-
-  @ViewChild('userForm') userForm: NgForm;
+  // model: User = new User(1, '', '', null);
+  // roles: string[] = ['Guest', 'Moderator', 'Administrator'];
+  //
+  // submitted = false;
+  //
+  // formErrors = {
+  //   name: '',
+  //   age: ''
+  // };
+  // validationMessages = {
+  //   name: {
+  //     required: 'Name is required.',
+  //     minlength: 'Name must be at least 4 characters.'
+  //   },
+  //   age: {
+  //     required: 'Age is required.'
+  //   }
+  // };
+  //
+  // @ViewChild('userForm') userForm: NgForm;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      login: new FormControl('User name', [Validators.required, Validators.minLength(10)]),
+      password: new FormControl('User password', [Validators.required, Validators.minLength(8)])
+    });
   }
 
-  ngAfterViewInit(): void {
-    this.userForm.valueChanges.subscribe((data) => this.onValueChanged());
+  onSubmit(form): void {
+    console.log(form);
   }
 
-  onValueChanged(data?: any): void {
-    if (!this.userForm) {
-      return;
-    }
-
-    const form = this.userForm.form;
-    for (const field in this.formErrors) {
-      this.formErrors[field] = '';
-
-      const control = form.get(field);
-
-      if (control && control.dirty && control.invalid) {
-        const message = this.validationMessages[field];
-
-        for (const key in control.errors) {
-          this.formErrors[field] += message[key];
-        }
-      }
-    }
-  }
+  // ngAfterViewInit(): void {
+  //   this.userForm.valueChanges.subscribe((data) => this.onValueChanged());
+  // }
+  //
+  // onValueChanged(data?: any): void {
+  //   if (!this.userForm) {
+  //     return;
+  //   }
+  //
+  //   const form = this.userForm.form;
+  //   for (const field in this.formErrors) {
+  //     this.formErrors[field] = '';
+  //
+  //     const control = form.get(field);
+  //
+  //     if (control && control.dirty && control.invalid) {
+  //       const message = this.validationMessages[field];
+  //
+  //       for (const key in control.errors) {
+  //         this.formErrors[field] += message[key];
+  //       }
+  //     }
+  //   }
+  // }
 
 // get diagnostic(): string {
 //   return JSON.stringify(this.model);
@@ -68,9 +78,9 @@ export class FormComponent implements OnInit, AfterViewInit {
 //   console.log(err);
 // }
 
-  onSubmit(): void {
-    this.submitted = true;
-    console.log('Form submitted');
-  }
+  // onSubmit(): void {
+  //   this.submitted = true;
+  //   console.log('Form submitted');
+  // }
 
 }
